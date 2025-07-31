7b0f0a50cb456ee15a5315c864c302be5dae6920
JosephHAtWork <193976820+JosephHAtWork@users.noreply.github.com>
2025-07-31 04:48:27 +0000

    Track linked symbols

--- surfactant/infoextractors/py_file.py
+++ surfactant/infoextractors/py_file.py
@@ -22,6 +22,7 @@ class PyImportedModule:
 class Schema:
     pyImports: Dict[str, PyImportedModule] = field(default_factory=dict)
     pyDefinedObjects: List[str] = field(default_factory=list)
+    pyLinkedObjects: Dict[str, str] = field(default_factory=dict)
 
 
 def supports_filetype(filetype: str) -> bool:
@@ -51,7 +52,7 @@ def extract_file_info(
     schema = Schema()
 
     extract_imports(schema, filename, project_root)
-    extract_defined_objects(schema, filename)
+    extract_ast_objects(schema, filename)
 
     return asdict(schema)
 
@@ -75,23 +76,37 @@ def extract_imports(schema: Schema, filename: str, project_root: str):
             schema.pyImports[moduleName].imported_objects.append(importName)
 
 
-def extract_defined_objects(schema: Schema, filename: str):
+def extract_ast_objects(schema: Schema, filename: str):
     with open(filename, "r") as f:
         content = f.read()
 
-        objects = []
         tree = ast.parse(content)
 
         for node in tree.body:
             if isinstance(node, (ast.ClassDef, ast.FunctionDef)):
-                objects.append(node.name)
+                schema.pyDefinedObjects.append(node.name)
             elif isinstance(node, ast.Assign):
                 for target in node.targets:
                     if isinstance(target, ast.Name):
-                        objects.append(target.id)
+                        schema.pyDefinedObjects.append(target.id)
+
+                        # Get all 'linked' objects: Symbols referenced in __all__ that were previously imported (typically found in __init__.py files)
+                        if target.id == "__all__":
+                            if isinstance(node.value, ast.List):
+                                # Caveat: this only works for constant values/strings.
+                                # If a function returns a name (like __all__ = [ "myVar", ret_myVar2() ]), it won't be detected
+                                # Only by loading the file will values like those be able to be extracted
+                                all_const_values = [
+                                    elt.s
+                                    for elt in node.value.elts
+                                    if isinstance(elt, ast.Constant)
+                                ]
+                                for name in all_const_values:
+                                    for pkg, value in schema.pyImports.items():
+                                        if name in value.imported_objects:
+                                            schema.pyLinkedObjects[name] = pkg
+
                     elif isinstance(target, ast.Tuple):  # eg a, b = (1, 2)
                         for elt in target.elts:
                             if isinstance(elt, ast.Name):
-                                objects.append(elt.id)
-
-        schema.pyDefinedObjects = objects
+                                schema.pyDefinedObjects.append(elt.id)
