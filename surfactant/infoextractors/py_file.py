1a045a81a3c8a30d1b628e892671a7eca83bbede
JosephHAtWork <193976820+JosephHAtWork@users.noreply.github.com>
2025-07-31 03:29:36 +0000

    Add defined object section to output

--- surfactant/infoextractors/py_file.py
+++ surfactant/infoextractors/py_file.py
@@ -1,3 +1,4 @@
+import ast
 import os
 from dataclasses import asdict, dataclass, field
 from sys import modules
@@ -20,6 +21,7 @@ class PyImportedModule:
 @dataclass
 class Schema:
     pyImports: Dict[str, PyImportedModule] = field(default_factory=dict)
+    pyDefinedObjects: List[str] = field(default_factory=list)
 
 
 def supports_filetype(filetype: str) -> bool:
@@ -46,12 +48,15 @@ def extract_file_info(
         )
     project_root = current_context.extractPaths[0]
 
-    return extract_imports(filename, filetype, project_root)
-
-
-def extract_imports(filename: str, filetype: str, project_root: str):
     schema = Schema()
 
+    extract_imports(schema, filename, project_root)
+    extract_defined_objects(schema, filename)
+
+    return asdict(schema)
+
+
+def extract_imports(schema: Schema, filename: str, project_root: str):
     isortConfig = Config()
     isortConfig.project_root = os.path.abspath(
         project_root
@@ -69,4 +74,24 @@ def extract_imports(filename: str, filetype: str, project_root: str):
         else:
             schema.pyImports[moduleName].imported_objects.append(importName)
 
-    return asdict(schema)
+
+def extract_defined_objects(schema: Schema, filename: str):
+    with open(filename, "r") as f:
+        content = f.read()
+
+        objects = []
+        tree = ast.parse(content)
+
+        for node in tree.body:
+            if isinstance(node, (ast.ClassDef, ast.FunctionDef)):
+                objects.append(node.name)
+            elif isinstance(node, ast.Assign):
+                for target in node.targets:
+                    if isinstance(target, ast.Name):
+                        objects.append(target.id)
+                    elif isinstance(target, ast.Tuple):  # eg a, b = (1, 2)
+                        for elt in target.elts:
+                            if isinstance(elt, ast.Name):
+                                objects.append(elt.id)
+
+        schema.pyDefinedObjects = objects
