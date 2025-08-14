2a545afe69a3b5d7592b545bb58053e1e76b37c6
JosephHAtWork <193976820+JosephHAtWork@users.noreply.github.com>
2025-08-14 18:36:37 +0000

    Add simple state management

---
+++ plugins/SBOMVis/customTemplates/src/state.js
@@ -0,0 +1,28 @@
+const defaultState = {
+
+};
+
+export const store = {
+	state: defaultState,
+	listeners: {},
+
+	set(k, v) {
+		this.state[k] = v;
+		if (this.listeners[k]) {
+			for (const callback of this.listeners[k]) {
+				callback(v);
+			}
+		}
+	},
+
+	get(k) {
+		return this.state[k];
+	},
+
+	subscribe(key, callback) {
+		if (!this.listeners[key]) {
+			this.listeners[key] = [];
+		}
+		this.listeners[key].push(callback);
+	},
+};
